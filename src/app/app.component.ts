1ebc8246a0de00c84a6ac114632c20097b0182ec
Barkha13 <barkha0292@yahoo.com>
2017-11-07 19:29:31 -0800

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,7 +14,7 @@ export class AppComponent {
 
   onSubmit(){
     this.users.push(this.user);
-    this.message = `Thank you for registering with us ${this.user.first_name}. We just sent you a confirmation email at ${this.user.email}, and we will send all mail to ${this.user.street_address}, ${this.user.city}, ${this.user.state}. Have a wonderful day!`;
+    this.message = "Thank you for registering with us ",this.user.first_name,". We just sent you a confirmation email at ",this.user.email,", and we will send all mail to ",this.user.street_address,this.user.city,this.user.state,". Have a wonderful day!";
     this.user = new User();
     
   }
