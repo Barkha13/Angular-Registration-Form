78f8728da16cf787dfc6466fe0011dcbc85d3424
Barkha13 <barkha0292@yahoo.com>
2017-11-07 19:26:22 -0800

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { User } from './user';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Registration';
+  user = new User();
+  users = [];
+  message = '';
+
+  onSubmit(){
+    this.users.push(this.user);
+    this.message = `Thank you for registering with us ${this.user.first_name}. We just sent you a confirmation email at ${this.user.email}, and we will send all mail to ${this.user.street_address}, ${this.user.city}, ${this.user.state}. Have a wonderful day!`;
+    this.user = new User();
+    
+  }
+}
---
+++ src/app/user.ts
@@ -0,0 +1,13 @@
+export class User{
+    constructor(
+    public first_name = '',
+    public last_name = '',
+    public email = '',
+    public password = '',
+    public street_address = '',
+    public unit_apt = '',
+    public city = '',
+    public state = '',
+    public lucky = ''
+    ){}
+}
